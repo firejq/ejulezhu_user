7a4f7491cc8c3f6bd68ed577c73d3a05a9c127a2
firejq <firejq@outlook.com>
2017-10-03 22:41:38 +0800

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by firejq on 2017-09-29.
+ */
+'use strict';
+
+angular.module('app', ['ui.router', 'ngCookies', 'validation', 'ngAnimate']);
+
---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var open = require('open');
+
+var app = {
+	srcPath: 'app/',
+	//devPath: 'build/',
+	prdPath: 'dist/'
+};
+
+gulp.task('lib', function() {
+	gulp.src('bower_components/**/*.js')
+		.pipe(gulp.dest(app.prdPath + 'vendor'))
+		.pipe($.connect.reload());
+});
+
+gulp.task('html', function() {
+	gulp.src(app.srcPath + '**/*.html')
+		.pipe(gulp.dest(app.prdPath))
+		.pipe($.connect.reload());
+});
+
+gulp.task('txt', function() {
+	gulp.src(app.srcPath + '*.txt')
+		.pipe(gulp.dest(app.prdPath))
+		.pipe($.connect.reload());
+});
+
+gulp.task('json', function() {
+	gulp.src(app.srcPath + 'data/**/*.json')
+		.pipe(gulp.dest(app.prdPath + 'data'))
+		.pipe($.connect.reload());
+});
+
+gulp.task('less', function() {
+	gulp.src(app.srcPath + 'styles/index.less')
+		.pipe($.plumber())
+		.pipe($.less())
+		.pipe($.cssmin())
+		.pipe(gulp.dest(app.prdPath + 'css'))
+		.pipe($.connect.reload());
+});
+
+gulp.task('js', function() {
+	gulp.src(app.srcPath + 'scripts/**/*.js')
+		//.pipe($.plumber())
+		.pipe($.concat('index.js'))
+		.pipe($.uglify())
+		.pipe(gulp.dest(app.prdPath + 'js'))
+		.pipe($.connect.reload());
+});
+
+gulp.task('image', function() {
+	gulp.src(app.srcPath + 'images/**/*')
+		.pipe($.plumber())
+		.pipe($.imagemin())
+		.pipe(gulp.dest(app.prdPath + 'images'))
+		.pipe($.connect.reload());
+});
+
+gulp.task('build', ['js', 'less', 'lib', 'txt', 'html', 'json', 'image']);
+
+gulp.task('clean', function() {
+	gulp.src([app.prdPath])
+		.pipe($.clean());
+});
+
+gulp.task('serve', ['build'], function() {
+	$.connect.server({
+		root: [app.prdPath],
+		livereload: true,
+		port: 9000
+	});
+
+	open('http://localhost:9000');
+
+	gulp.watch('bower_components/**/*', ['lib']);
+	gulp.watch(app.srcPath + '**/*.html', ['html']);
+	gulp.watch(app.srcPath + 'data/**/*.json', ['json']);
+	gulp.watch(app.srcPath + 'styles/**/*.less', ['less']);
+	gulp.watch(app.srcPath + 'scripts/**/*.js', ['js']);
+	gulp.watch(app.srcPath + 'images/**/*', ['image']);
+});
+
+gulp.task('default', ['clean', 'serve']);
