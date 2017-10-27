ad6642d6521da3b1bf10238e25b476428b3d2cfa
firejq <firejq@outlook.com>
2017-10-27 15:03:14 +0800

    finished redPackage module

--- app/scripts/controller/redPackageCtrl.js
+++ app/scripts/controller/redPackageCtrl.js
@@ -5,77 +5,125 @@
 
 angular.module('app').controller('redPackageCtrl', ['$scope', '$http', 'cache', function ($scope, $http, cache) {
 
+	//初始化变量
 	var mobilenoCookie = cache.get('Mobileno');
 	var tokenCookie = cache.get('Token');
+
+	$scope.redPackageData = {
+		score: 0,//抢到了多少积分
+		visible: {
+			anticipate: true,//默认显示抢红包图案
+			sad: false,//伤心页默认不显示
+			happy: false,//高兴页默认不显示
+			rule: false//规则页面默认不显示
+		},
+	};
 
 	/**
 	 * 分享次数加1，增加一次抢红包机会
 	 */
-	$http({
-		method: 'GET',
-		url: $scope.global.url + 'redpackage/share',
-		params: {
-			Mobileno: mobilenoCookie,
-			Token: tokenCookie,
-			Reqtime: Math.round(new Date().getTime()/1000),
-			Usertype: 1,
-			Channel: 'weixin' //分享渠道，如weixin
-		}
-	}).then(function (response) {
-		if (response.data.status === 0) {
-			console.log('add chance successfully');
-		}
-	}, function (response) {
-		console.log('fail! ' + response);
-	});
+	$scope.share = function () {
+		$http({
+			method: 'GET',
+			url: $scope.global.url + 'redpackage/share',
+			params: {
+				Mobileno: mobilenoCookie,
+				Token: tokenCookie,
+				Reqtime: Math.round(new Date().getTime()/1000),
+				Usertype: 1,
+				Channel: 'weixin' //分享渠道，如weixin
+			}
+		}).then(function (response) {
+			if (response.data.status === 0) {
+				//TODO 分享的页面还没做
+				//console.log('Shared! Add chance successfully');
+			}
+		}, function (response) {
+			console.log('fail! ' + response);
+		});
+	};
 
 
 	/**
-	 * 获取当前还有多少次机会抢红包
+	 * 抢红包
 	 */
-	$http({
-		method: 'GET',
-		url: $scope.global.url + 'redpackage/chance',
-		params: {
-			Mobileno: mobilenoCookie,
-			Token: tokenCookie,
-			Reqtime: Math.round(new Date().getTime()/1000),
-			Usertype: 1
-		}
-	}).then(function (response) {
-		if (response.data.status === 0) {
-			console.log('Chance: ' + response.data.Chance);
-		} else {
-			$scope.global.msg('获取信息出错');
-		}
-	}, function (response) {
-		console.log('fail! ' + response);
-	});
+	$scope.grabRedPackage = function () {
+		//获取当前还有多少次机会抢红包
+		$http({
+			method: 'GET',
+			url: $scope.global.url + 'redpackage/chance',
+			params: {
+				Mobileno: mobilenoCookie,
+				Token: tokenCookie,
+				Reqtime: Math.round(new Date().getTime()/1000),
+				Usertype: 1
+			}
+		}).then(function (response) {
+			if (response.data.status === 0) {
+				//console.log('Chance: ' + response.data.Chance);
+				if (response.data.Chance > 0) {
+
+					//执行抢红包操作
+					$http({
+						method: 'GET',
+						url: $scope.global.url + 'redpackage/grad',
+						params: {
+							Mobileno: mobilenoCookie,
+							Token: tokenCookie,
+							Reqtime: Math.round(new Date().getTime()/1000),
+							Usertype: 1
+						}
+					}).then(function (response) {
+						//console.log(response);
+						if (response.data.status === 0) {
+							if (response.data.Type === 0) {
+								//抢到了红包
+								//console.log('you get the money:' + response.data.Value);
+								$scope.redPackageData.score = response.data.Value;
+
+								//隐藏入口页，显示高兴页（抢到积分大于0）/伤心页（抢到0积分）
+								$scope.redPackageData.visible.anticipate = false;
+								if ($scope.redPackageData.score === 0) {
+									$scope.redPackageData.visible.happy = false;
+									$scope.redPackageData.visible.sad = true;
+								} else {
+									$scope.redPackageData.visible.sad = false;
+									$scope.redPackageData.visible.happy = true;
+								}
+							}
+						} else {
+							$scope.global.msg('操作出错');
+							layer.closeAll();
+						}
+					}, function (response) {
+						console.log('fail! ' + response);
+					});
+
+				} else {
+					//没有抢红包的机会了
+					$scope.global.msg('抱歉，你抢红包的机会用完啦~');
+				}
+			} else {
+				$scope.global.msg('获取信息出错');
+			}
+		}, function (response) {
+			console.log('fail! ' + response);
+		});
+	};
 
 
 	/**
-	 * 执行抢红包操作
+	 * 显示规则页面
 	 */
-	$http({
-		method: 'GET',
-		url: $scope.global.url + 'redpackage/grad',
-		params: {
-			Mobileno: mobilenoCookie,
-			Token: tokenCookie,
-			Reqtime: Math.round(new Date().getTime()/1000),
-			Usertype: 1
-		}
-	}).then(function (response) {
-		if (response.data.status === 0) {
-			console.log('you get the money:' + response.data.Value);
-		} else {
-			$scope.global.msg('操作出错');
-			layer.closeAll();
-		}
-	}, function (response) {
-		console.log('fail! ' + response);
-	});
-
+	$scope.showRule = function () {
+		$scope.redPackageData.visible.rule = true;
+	};
+	/**
+	 * 隐藏规则页面
+	 */
+	$scope.hideRule = function () {
+		$scope.redPackageData.visible.rule = false;
+	};
 
 
 }]);
--- app/scripts/controller/repairBookingCtrl.js
+++ app/scripts/controller/repairBookingCtrl.js
@@ -11,11 +11,14 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 
 
 	$scope.furnitureRepairSubmitData = {
-		fixType: '',//已选择维修类型，多个用逗号分隔
+		fixType: '',//已选择维修类型，多个用空格分隔
+		fixTypeSubmit: $state.params.types,//已选择维修类型，多个用逗号分隔
 		materialPrices: [],//已选择类型对应的所有维修材料，每一种材料包括描述、单价、Id、单位
 		labourPrices: [],//已选择类型对应的所有人工费，每一种人工费包括描述、单价、Id
-		selectedMaterial: [],//已选择的维修材料（Id和数量)，如[{materialId:1,number:2}, {materialId:2,number:1}]
-		selectedLabour: [],//已选择的维修人工费（Id和数量），如[{labourId:1,number:2}, {labourId:2,number:1}]
+		selectedMaterial: '',//已选择的维修材料（Id和数量)，格式为：materialId:number,如 1:1,2:5
+		materialPricesTotal: 0,//维修材料费合计
+		selectedLabour: '',//已选择的维修人工费（Id和数量），如[{labourId:1,number:2}, {labourId:2,number:1}]
+		labourPricesTotal: 0,//人工费合计
 		appointTime: '',//上门时间
 		addrId: 0,//地址Id
 		address: '',//地址
@@ -31,9 +34,23 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 
 
 	/**
-	 * 总价估价按钮触发函数
+	 * 点击维修人工费按钮的跳转函数
+	 */
+	$scope.labourPricesLink = function () {
+		console.log($scope.furnitureRepairSubmitData.materialPricesTotal);
+		if ($scope.furnitureRepairSubmitData.materialPricesTotal === 0) {
+			$scope.global.msg('请先选择维修材料费');
+		} else {
+			$state.go('repairBooking.labourPrice');
+		}
+	};
+
+
+	/**
+	 * 总价估价按钮函数
 	 */
 	$scope.evaluate = function () {
+		$scope.furnitureRepairSubmitData.total = ($scope.furnitureRepairSubmitData.materialPricesTotal + $scope.furnitureRepairSubmitData.labourPricesTotal) * 1.2
 		//TODO
 	};
 
@@ -67,14 +84,15 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 							describe: response.data[keyName].materialprices[i].Desc,
 							id: response.data[keyName].materialprices[i].Id,
 							price: response.data[keyName].materialprices[i].Price,
-							unit: response.data[keyName].materialprices[i].Unit
+							unit: response.data[keyName].materialprices[i].Unit,
+							selected: 0//选择的数量
 						});
 					}
 
-
 					$scope.furnitureRepairSubmitData.labourPrices.push({
 						typeName: keyName,//维修类型名称
-						kinds: []//该类型拥有的所有种类
+						kinds: [],//该类型拥有的所有种类
+						selectedLabourId: ''//该类型选择的人工费id，为空表示未选择
 					});
 					//计算数组的最后一项的索引，即当前 keyName 对应的项
 					var lastIndexOfLabour = $scope.furnitureRepairSubmitData.labourPrices.length - 1;
@@ -82,13 +100,11 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 						$scope.furnitureRepairSubmitData.labourPrices[lastIndexOfLabour].kinds.push({
 							describe: response.data[keyName].labourprices[i].Desc,
 							id: response.data[keyName].labourprices[i].Id,
-							price: response.data[keyName].labourprices[i].Price
+							price: response.data[keyName].labourprices[i].Price,
 						});
 					}
 				}
 			}
-
-
 			console.log($scope.furnitureRepairSubmitData);
 
 		} else {
@@ -127,7 +143,7 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 			}
 			//console.log($scope.bookingSubmitData.address);
 		} else {
-			$scope.global.cancel('获取地址失败');
+			$scope.global.msg('获取地址失败');
 		}
 
 	}, function (response) {
@@ -172,7 +188,7 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 				if (response.data.Status === 0) {
 					//console.log('upload successfully');
 					//获取服务器回调信息：图片Id和外链地址，并存储到 imgInfoList 中
-					$scope.bookingSubmitData.imgInfoList.push({
+					$scope.furnitureRepairSubmitData.imgInfoList.push({
 						Id: response.data.Id,
 						Url: $scope.global.ip + response.data.Url
 					});
@@ -215,7 +231,7 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 				},
 				transformRequest: angular.identity
 			}).then(function (response) {
-				console.log(response);
+				//console.log(response);
 				if (response.data.Status === 0) {
 					//console.log('upload successfully');
 					//获取服务器回调信息：将语音id存储到 $scope.bookingSubmitData.voiceId 中
@@ -230,6 +246,213 @@ angular.module('app').controller('repairBookingCtrl', ['$scope', '$http', 'cache
 			});
 		}
 	});
+
+
+	/****************************************************
+   * 维修材料费明细子页面的控制代码
+	 ****************************************************/
+
+	/**
+	 * 执行对象的浅复制
+	 */
+	var shallowClone = function (myObj) {
+		if(typeof(myObj) !== 'object' || myObj === null) return myObj;
+		var newObj = {};
+		for(var i in myObj){
+			newObj[i] = shallowClone(myObj[i]);
+		}
+		return newObj;
+	};
+
+	/**
+	 * 点击维修材料的回调函数：显示模态框
+	 */
+	$scope.showModalOfMaterial = function (materialPrice) {
+
+		//对象的浅复制
+		$scope.currentMaterialPrice = shallowClone(materialPrice);
+
+		$('#modal-'+materialPrice.$$hashKey[7]).removeClass('ng-hide');
+
+	};
+	/**
+	 * 点击模态框任意地方的回调函数，隐藏模态框
+	 */
+	$scope.hideModalOfMaterial = function (materialPrice) {
+		$('#modal-'+materialPrice.$$hashKey[7]).addClass('ng-hide');
+	};
+	/**
+	 * 确认按钮触发函数
+	 */
+	$scope.materialPriceConfirm = function () {
+
+		//将修改后的值赋值到显示信息中
+		for (var i = 0, lenI = $scope.furnitureRepairSubmitData.materialPrices.length; i < lenI; i ++) {
+			if ($scope.furnitureRepairSubmitData.materialPrices[i].$$hashKey === $scope.currentMaterialPrice.$$hashKey) {
+				for (var kind in $scope.currentMaterialPrice.kinds) {
+					if ($scope.currentMaterialPrice.kinds[kind].selected > 0) {
+						$scope.furnitureRepairSubmitData.materialPrices[i].kinds[kind].selected = $scope.currentMaterialPrice.kinds[kind].selected;
+						//格式化提交预约的数据,格式为：materialId:number,如 1:1,2:5
+						if ($scope.furnitureRepairSubmitData.selectedMaterial === '') {
+							$scope.furnitureRepairSubmitData.selectedMaterial = $scope.currentMaterialPrice.kinds[kind].id + ':' + $scope.currentMaterialPrice.kinds[kind].selected;
+						} else {
+							$scope.furnitureRepairSubmitData.selectedMaterial += ',' + $scope.currentMaterialPrice.kinds[kind].id + ':' + $scope.currentMaterialPrice.kinds[kind].selected;
+						}
+
+					}
+				}
+				break;
+			}
+		}
+
+		//计算维修材料费明细的合计
+		for (var i = 0, lenI = $scope.furnitureRepairSubmitData.materialPrices.length; i < lenI; i ++) {
+			for (var j = 0, lenJ = $scope.furnitureRepairSubmitData.materialPrices[i].kinds.length; j < lenJ; j ++) {
+				if ($scope.furnitureRepairSubmitData.materialPrices[i].kinds[j].selected > 0) {
+					$scope.furnitureRepairSubmitData.materialPricesTotal += $scope.furnitureRepairSubmitData.materialPrices[i].kinds[j].price * $scope.furnitureRepairSubmitData.materialPrices[i].kinds[j].selected;
+				}
+			}
+		}
+
+		//隐藏modal层
+		$('#modal-'+$scope.currentMaterialPrice.$$hashKey[7]).addClass('ng-hide');
+	};
+	/********************************************************/
+
+
+
+
+
+
+	/********************************************************
+	 * 维修人工费明细子页面的控制代码
+	 *******************************************************/
+
+	/**
+	 * 点击维修材料的回调函数：显示模态框
+	 */
+	$scope.showModalOfLabour = function (labourPrice) {
+
+		$scope.currentLabourPrice = labourPrice;
+
+		$('#modal-'+labourPrice.$$hashKey[7]).removeClass('ng-hide');
+
+	};
+	/**
+	 * 点击模态框任意地方的回调函数，隐藏模态框
+	 */
+	$scope.hideModalOfLabour = function (labourPrice) {
+		$('#modal-'+labourPrice.$$hashKey[7]).addClass('ng-hide');
+	};
+	/**
+	 * 确认按钮触发函数
+	 */
+	$scope.labourPriceConfirm = function (labourPriceId) {
+
+		//将修改后的值赋值到显示信息中
+		for (var i = 0, lenI = $scope.furnitureRepairSubmitData.labourPrices.length; i < lenI; i ++) {
+			if ($scope.furnitureRepairSubmitData.labourPrices[i].$$hashKey === $scope.currentLabourPrice.$$hashKey) {
+				$scope.furnitureRepairSubmitData.labourPrices[i].selectedLabourId = labourPriceId;
+				//格式化提交预约的数据,格式为：labourId:number,如 1:1,2:5
+				if ($scope.furnitureRepairSubmitData.selectedLabour === '') {
+					$scope.furnitureRepairSubmitData.selectedLabour = labourPriceId + ':1';
+				} else {
+					$scope.furnitureRepairSubmitData.selectedLabour += ',' + labourPriceId + ':1';
+				}
+				break;
+			}
+		}
+
+		//计算维修材料费明细的合计
+		$scope.furnitureRepairSubmitData.labourPricesTotal = 0;
+		for (var i = 0, lenI = $scope.furnitureRepairSubmitData.labourPrices.length; i < lenI; i ++) {
+			var curLabourPriceId = $scope.furnitureRepairSubmitData.labourPrices[i].selectedLabourId;
+			for (var j = 0, lenJ = $scope.furnitureRepairSubmitData.labourPrices[i].kinds.length; j < lenJ; j ++) {
+				if ($scope.furnitureRepairSubmitData.labourPrices[i].kinds[j].id === curLabourPriceId) {
+					$scope.furnitureRepairSubmitData.labourPricesTotal += $scope.furnitureRepairSubmitData.labourPrices[i].kinds[j].price;
+					break;
+				}
+			}
+		}
+
+		//隐藏modal层
+		$('#modal-'+$scope.currentLabourPrice.$$hashKey[7]).addClass('ng-hide');
+	};
+
+
+
+
+	/******************************************************/
+
+
+	/**
+	 * 提交订单
+	 */
+	$scope.repairBookingOrderSubmit = function () {
+		if ($scope.furnitureRepairSubmitData.appointTime === '') {
+			$scope.global.msg('请选择上门时间');
+			return;
+		}
+		if ($scope.furnitureRepairSubmitData.attact === '') {
+			$scope.global.msg('请留言');
+			return;
+		}
+
+		//格式化图片id字符串
+		var imageIds = '';
+		for (var i = 0, len = $scope.furnitureRepairSubmitData.imgInfoList.length; i < len; i ++) {
+			if (i === 0) {
+				imageIds += $scope.furnitureRepairSubmitData.imgInfoList[i].Id;
+				continue;
+			}
+			imageIds += ',' + $scope.furnitureRepairSubmitData.imgInfoList[i].Id;
+		}
+
+
+		$http({
+			url: $scope.global.url + 'order/place',
+			method: 'GET',
+			params: {
+				Mobileno: mobilenoCookie,
+				Token: tokenCookie,
+				Usertype: 1,
+				Reqtime: Math.round(new Date().getTime()/1000),//10位unix时间戳
+				ImageId: imageIds,//图片文件id
+				VoiceId: '',//语音文件id
+				TotalPrice: $scope.furnitureRepairSubmitData.total,
+				AddrId: $scope.furnitureRepairSubmitData.addrId,//维修地址Id
+				AppintmentTime: (new Date($scope.furnitureRepairSubmitData.appointTime).getTime())/1000,//unix时间戳
+				Attact: $scope.furnitureRepairSubmitData.attact,//留言
+				FixType: $scope.furnitureRepairSubmitData.fixTypeSubmit,//维修类型
+				LabourId: $scope.furnitureRepairSubmitData.selectedLabour,//人工Id
+				MaterialId: $scope.furnitureRepairSubmitData.selectedMaterial//材料Id
+
+			}
+		}).then(function (response) {
+
+			console.log(response);
+			if (response.data.Status === 0) {
+				$scope.global.msg('提交成功');
+				$state.go('myOrderDetail', {orderId: response.data.Orderid, orderNo: response.data.Orderno});
+
+
+			} else {
+				$scope.global.msg('提交出错');
+			}
+		}, function (response) {
+			console.log('fail! ' + response);
+			$scope.global.msg('连接超时');
+		});
+
+
+
+
+
+
+
+	};
+
+
 
 
 
